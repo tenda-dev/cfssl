9f601a8273f21fcd197c710e4538611475fc43c8
Tendaworld <50552789+tendaworld@users.noreply.github.com>
2023-12-10 15:02:58 +0300

    Update 001_CreateCertificates.sql
    Fix postgres migration

--- certdb/pg/migrations/001_CreateCertificates.sql
+++ certdb/pg/migrations/001_CreateCertificates.sql
@@ -16,27 +16,31 @@ CREATE TABLE certificates (
   request                  bytea,
   PRIMARY KEY(serial_number, authority_key_identifier)
 );
-DO $$
-BEGIN
-IF NOT EXISTS (
-    SELECT 1
-    FROM pg_indexes
-    WHERE LOWER(indexname) = LOWER('certificates_created_at')
-    ) THEN
-    CREATE INDEX certificates_created_at ON certificates (created_at);
-END IF;
-END$$;
+DO 
+  '
+  BEGIN
+  IF NOT EXISTS (
+      SELECT 1
+      FROM pg_indexes
+      WHERE LOWER(indexname) = LOWER('certificates_created_at')
+      ) THEN
+      CREATE INDEX certificates_created_at ON certificates (created_at);
+  END IF;
+  END;
+  '  
 ;
-DO $$
-BEGIN
-IF NOT EXISTS (
-    SELECT 1
-    FROM pg_indexes
-    WHERE LOWER(indexname) = LOWER('certificates_revoked_at')
-    ) THEN
-    CREATE INDEX certificates_revoked_at ON certificates (revoked_at);
-END IF;
-END$$;
+DO
+  '
+  BEGIN
+  IF NOT EXISTS (
+      SELECT 1
+      FROM pg_indexes
+      WHERE LOWER(indexname) = LOWER('certificates_revoked_at')
+      ) THEN
+      CREATE INDEX certificates_revoked_at ON certificates (revoked_at);
+  END IF;
+  END;
+  '
 ;
 
 CREATE TABLE ocsp_responses (
